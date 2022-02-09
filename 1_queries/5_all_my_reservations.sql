2f39dfde3d4ac69e60b1f99f1c7b8a4b2a5917c5
brandonDahlberg <brandon.dahlberg21@gmail.com>
2022-02-09 18:11:24 +0000

    Set up the JavaScript project

--- 1_queries/5_all_my_reservations.sql
+++ 1_queries/5_all_my_reservations.sql
@@ -1,10 +1,9 @@
-SELECT properties.id, properties.title, reservations.start_date, properties.cost_per_night, AVG(property_reviews.rating) as avg_rating
-FROM properties
-JOIN reservations on reservations.property_id = properties.id
-JOIN property_reviews on property_reviews.property_id = reservations.property_id
-JOIN users on property_reviews.reservation_id = reservations.id
+SELECT properties.*, reservations.*, avg(rating) as average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id
 WHERE reservations.guest_id = 1
 AND reservations.end_date < now()::date
-GROUP BY properties.id, reservations.start_date
+GROUP BY properties.id, reservations.id
 ORDER BY reservations.start_date
 LIMIT 10;
